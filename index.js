880353c5d8fe861f61a4c8e1fb181ab7cc05343f
Renan Menezes de Matos <renanmenezesdematos@gmail.com>
2023-04-11 21:01:52 -0300

    inicio criação do JavaScript

---
+++ index.js
@@ -0,0 +1,7 @@
+let registerGoal = document.getElementById('container-register-goals')
+
+let callPopUp = function (){
+    console.log('fsdfsd')
+}
+
+registerGoal.addEventListener('click', callPopUp)
